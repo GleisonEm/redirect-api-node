8e4db65948775936f400780f7ef76e40ebb3a004
Gleison Emanuel <gleison.spfc1208@hotmail.com>
2023-06-04 17:46:03 -0300

    pedro

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ const app = express()
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs");
 
-app.get("/redirecionar", (req, res) => {
-  res.redirect(
-    "http://145.14.134.34:3021/api/users/download/Gleison-Emanuel-1685842562150-87974006609.mp4"
-  );
+app.get("/redirect", (req, res) => {
+  const fileId = req.params.fileId;
+
+  if (!fileId) {
+    res.status(422).send({ message: "File ID is required" });
+  }
+
+  return res.redirect("http://145.14.134.34:3021/api/users/download/" + fileId);
 });
 
 app.get("/", (req, res) => res.render("pages/index"));
