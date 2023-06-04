4dfba43a050de942102379c652dd256b521cf7bf
Gleison Emanuel <gleison.spfc1208@hotmail.com>
2023-06-04 17:22:16 -0300

    adjusts

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/redirecionar", (req, res) => {
   res.redirect("https://www.google.com.br/");
 });
 
+app.get("/", (req, res) => {
+  res.send({ message: "https://www.google.com.br/" });
+});
+
 app.listen(3030, () => {
   console.log("Servidor iniciado na porta 3000");
 });
