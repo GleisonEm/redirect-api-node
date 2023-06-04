8ae5fc077b8f1c5e008add616cc66c316bccd1d0
Gleison Emanuel <gleison.spfc1208@hotmail.com>
2023-06-04 17:15:56 -0300

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/redirecionar", (req, res) => {
+  res.redirect("https://www.google.com.br/");
+});
+
+app.listen(3030, () => {
+  console.log("Servidor iniciado na porta 3000");
+});
